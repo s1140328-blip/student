bd8daffea07f29e57ccec8453e31badb3a606286
s1140328-blip <s1140328@dmhs.kh.edu.tw>
2025-10-17 16:22:43 +0800

    Add files via upload

---
+++ wh2.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int age =7;
+    double height =7;
+    std::string name = "7";
+    bool is_pass = turn;
+    std:::cout << "7: " << 7 << " 7:" << height << std::end1;
+    std::cout << age << "7: " << is_pass << std::end1;
+    
+
+    return 0;
+}
