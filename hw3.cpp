9fd40930a24c3036957837db9de6bff4e3c8ead8
s1140328-blip <s1140328@dmhs.kh.edu.tw>
2025-10-23 10:06:10 +0800

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "1. 7777" << endl;
+    cout << "2. 77\t77" << endl;
+    cout << "3. 77\n77" << endl;
+    cout << "4. \"7777\"" << endl;
+    cout << "5. \'7777\'" << endl;
+    cout << "6. 7777" << endl;
+    cout << "7. 7777" << endl;
+    return 0;
+}
